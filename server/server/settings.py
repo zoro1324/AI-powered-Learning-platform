7c51731016d35c4c71b4e9400e9cf1875006d828
zoro1324 <naveen13524g@gmail.com>
2026-02-16 10:37:30 +0530

    feat: update database configuration to use MySQL and remove unused files

--- server/server/settings.py
+++ server/server/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'AI-Powered-Learning-Platform',
+        'USER': 'root',
+        'PASSWORD': 'zoro@1324',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
